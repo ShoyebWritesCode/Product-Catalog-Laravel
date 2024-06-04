a34c96d91c382a328e0831c8b76f81236d66c086
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2024-06-04 19:06:13 +0600

    Added migration scripts

--- database/migrations/2024_06_04_124354_create_products__table.php
+++ database/migrations/2024_06_04_124354_create_products__table.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('usertype')->default('user');
-            $table->rememberToken();
+            $table->integer ('total');
             $table->timestamps();
+            $table->foreignId('catagory_id')->constrained('catagories');
+            $table->foreignId('subcatagory_id')->constrained('subcatagories');
         });
     }
 
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
