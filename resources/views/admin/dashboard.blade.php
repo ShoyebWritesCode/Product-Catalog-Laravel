16e84a07d7d6ef5536e40d61b5d70b333d3302c0
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2024-06-07 11:10:32 +0600

    Add Frontend Routes

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,25 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Admin Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+        <div class="flex justify-between items-center">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Admin Dashboard') }}
+            </h2>
+            <div class="flex-1 text-center">
+                <a href="{{ route('admin.product.home') }}" class="text-blue-500 hover:text-blue-700 mx-4">
+                    {{ __('Product') }}
+                </a>
+                <a href="{{ route('admin.catagory.home') }}" class="text-blue-500 hover:text-blue-700 mx-4">
+                    {{ __('Catagory') }}
+                </a>
             </div>
         </div>
+    </x-slot>
+    
+    
+
+    <div class="flex flex-wrap gap-6">
+        <img src="{{ asset('images/admin.jpg') }}" alt="Admin Dashboard" class="w-full h-32 object-cover rounded-md mb-2">
     </div>
+    
+    
 </x-app-layout>
