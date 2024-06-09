ff67aa73bea0a705615dbca09a2bcf1ea2c68d58
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2024-06-09 12:53:26 +0600

    Add user Method in Review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -15,5 +15,9 @@ class Review extends Model
     {
         return $this->belongsTo(Product::class, 'product_id');
     }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
     
 }
