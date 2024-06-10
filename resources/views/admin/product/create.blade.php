b635f40ea2ea70636f409c43905feb75b75d56e0
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2024-06-10 18:15:55 +0600

    Add Fields for Multiple Images

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -64,12 +64,15 @@
                             </div>
                         </div>
                         <div class="mb-4">
-                            <label for="image" class="block text-sm font-medium text-gray-700">Image:</label>
+                            <label for="image" class="block text-sm font-medium text-gray-700">Images:</label>
                             <input type="file" name="image" id="image" class="mt-1 p-2 w-full border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200 focus:border-indigo-500">
                             @error('image')
                                 <span class="text-red-500">{{ $message }}</span>
                             @enderror
+                            <input type="file" name="image1" id="image" class="mt-1 p-2 w-full border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200 focus:border-indigo-500">
+                            <input type="file" name="image2" id="image" class="mt-1 p-2 w-full border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200 focus:border-indigo-500">
                         </div>
+                        
                         
                         <button type="submit" class="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">Add</button>
                     </form>
