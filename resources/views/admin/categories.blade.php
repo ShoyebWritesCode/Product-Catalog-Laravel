f955522aed9221957f64d23c9c42b365ca0dcee0
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2024-07-30 11:35:45 +0600

    Add Banner from Admin Dashboard

--- resources/views/admin/categories.blade.php
+++ resources/views/admin/categories.blade.php
@@ -23,13 +23,39 @@
     </div>
     <div class="row">
         <div class="col-12">
-            <x-table :headers="['ID', 'Category', 'Subcategories']" :rows="$categories->map(function ($category) use ($subcategories) {
-                $subcategoryNames = $subcategories
-                    ->where('parent_id', $category->id)
-                    ->pluck('name')
-                    ->implode('<br>');
-                return [$category->id, $category->name, $subcategoryNames];
-            })" />
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Category</th>
+                        <th>Subcategories</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($categories as $category)
+                        <tr>
+                            <td>{{ $category->id }}</td>
+                            <td>{{ $category->name }}</td>
+                            <td>
+                                @php
+                                    $subcategoryNames = $subcategories
+                                        ->where('parent_id', $category->id)
+                                        ->pluck('name')
+                                        ->implode('<br>');
+                                @endphp
+                                {!! $subcategoryNames !!}
+                            </td>
+                            <td>
+                                <a href="{{ route('admin.category.edit', $category->id) }}"
+                                    class="btn btn-sm btn-warning mr-2" title="Edit">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
 
     </div>
